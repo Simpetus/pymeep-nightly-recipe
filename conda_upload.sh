a9ef45173a1c4868f658e658c1cad0d4b75ddaef
Chris Hogan <c096h800@gmail.com>
2019-06-10 13:54:33 -0500

    Use simpetus channel

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u chogan --label $1 $HOME/miniconda/conda-bld/**/pymeep-*.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u simpetus --label $1 $HOME/miniconda/conda-bld/**/pymeep-*.tar.bz2 --force
