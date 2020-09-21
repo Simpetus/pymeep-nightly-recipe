8d3ff29d237071678c459bd38e021c5467ce6e9a
Robin Dunn <robin@simpetus.com>
2020-09-21 13:32:15 -0700

    Only muck around with the OSX SDK when running on Travis

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,14 +7,16 @@ export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 # We need to install a later MacOS SDK than the default Travis SDK for compatibilty with the Anaconda compilers.
 if [[ $(uname) == Darwin ]]; then
     export MACOSX_DEPLOYMENT_TARGET="10.9"
-    export CONDA_BUILD_SYSROOT="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
-    echo "Downloading ${MACOSX_DEPLOYMENT_TARGET} sdk"
-    curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz
-    tar -xf MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz -C "$(dirname "$CONDA_BUILD_SYSROOT")"
-    # set minimum sdk version to our target
-    plutil -replace MinimumSDKVersion -string ${MACOSX_DEPLOYMENT_TARGET} $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
-    plutil -replace DTSDKName -string macosx${MACOSX_DEPLOYMENT_TARGET}internal $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
     export LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
+    if [[ $TRAVIS = true ]]; then
+        export CONDA_BUILD_SYSROOT="$(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
+        echo "Downloading ${MACOSX_DEPLOYMENT_TARGET} sdk"
+        curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz
+        tar -xf MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz -C "$(dirname "$CONDA_BUILD_SYSROOT")"
+        # set minimum sdk version to our target
+        plutil -replace MinimumSDKVersion -string ${MACOSX_DEPLOYMENT_TARGET} $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
+        plutil -replace DTSDKName -string macosx${MACOSX_DEPLOYMENT_TARGET}internal $(xcode-select -p)/Platforms/MacOSX.platform/Info.plist
+    fi
 fi
 
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
