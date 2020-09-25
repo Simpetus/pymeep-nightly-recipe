19f5a045af34ff94bd22810d2b5ef6e07fa8ff4a
Robin Dunn <robin@alldunn.com>
2020-09-25 14:03:16 -0700

    recipe updates (#14)
    * Allow the newest version of SWIG to be used
    
    * experiment with reduced version specific dependencies
    
    * turn on xtrace and  errexit for debugging
    
    * Only muck around with the OSX SDK when running on Travis
    
    * Add some more build tools
    
    * Add build configs from pymeep-feedstock
    
    * move builds tools to the `host:` section
    
    * bump build number
    
    * Restructure the Travis builds, now more parameter-driven
    
    * Empty commit to trigger CI
    
    * Empty commit to trigger CI
    
    * Fix typo in path name
    
    * Try a different way to test if running on travis
    
    * Use $HOME to check for Travis. Conda-build seems to clear some env variables
    
    * Expand the build matrix to cover Linux|OSX x Py3.6|3.7|3.8 x nompi|mpich
    
    * Fix env var assigment
    
    * bump build number
    
    * Switch to xenial for Linux builds
    
    * tell `parallel` to print the commands being executed
    
    * Empty commit to trigger CI
    
    * Run the tests with pytest, which should help reduce the noise and better identify problems.
    
    * Try running one test module at a time, without pytest
    
    * Remove pytest dependency
    
    Co-authored-by: Robin Dunn <>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "**** run_test.sh starting"
+echo "**** mpi: $mpi"
+echo "**** python: $($PYTHON --version)"
+
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
     for t in python/tests/*.py; do
         if [ "$(basename $t)" != "material_dispersion.py" -a "$(basename $t)" != "mpb.py" ]; then
-            echo "Running $(basename $t)"
+            echo "**** Running $(basename $t)"
             OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
         fi
     done
@@ -11,6 +15,12 @@ else
     # Mac builds are over the 50 minute time limit on Travis. Skip the serial
     # tests until we find a way to speed things up
     if [[ $(uname) == Linux ]]; then
-        OPENBLAS_NUM_THREADS==1 find python/tests -name "*.py" | sed /mpb/d | parallel "$PYTHON {}"
+        # OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel -v "$PYTHON {}"
+
+        for t in $(find python/tests -name "*.py" | sed /mpb/d); do
+            echo "**** Running $(basename $t)"
+            OPENBLAS_NUM_THREADS=1 $PYTHON $t
+        done
+
     fi
 fi
