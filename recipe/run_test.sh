319ae055da1e286e148d961c4af668d06718f0b7
Robin Dunn <robin@simpetus.com>
2020-09-29 14:23:34 -0700

    Run nompi tests without parallel

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,7 +7,7 @@ echo "**** python: $($PYTHON --version)"
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
     for t in python/tests/*.py; do
         if [ "$(basename $t)" != "material_dispersion.py" -a "$(basename $t)" != "mpb.py" ]; then
-            echo "**** Running $(basename $t)"
+            echo "Running $(basename $t)"
             OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
         fi
     done
@@ -17,6 +17,11 @@ else
     if [[ $(uname) == Linux ]]; then
         export OPENBLAS_NUM_THREADS=1
         export OMP_NUM_THREADS=1
-        find python/tests -name "*.py" | sed /mpb/d | parallel -v "$PYTHON {}"
+        #find python/tests -name "*.py" | sed /mpb/d | parallel -v "$PYTHON {}"
+
+        for t in $(find python/tests -name "*.py" | sed /mpb/d); do
+            echo "Running $(basename $t)"
+            $PYTHON $t
+        done
     fi
 fi
