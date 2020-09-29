46663c46ab9f0e6495e3084a89d93a51b26c3fd6
Robin Dunn <robin@simpetus.com>
2020-09-29 16:40:25 -0700

    Update recipe again (#16)
    * Bump the build number
    
    * Specify fortran compiler details
    
    * Fix license name to match names conda-forge allows
    
    * Move build tools to build section, add some additional tools
    
    * Remove Smithy files from conda-config, they're no longer needed here
    
    * Switch back to running tests with parallel
    
    * Update TravisCI config now that build dependencies are working correctly
    
    * try restoring the version constraints for libctl and mpb
    
    * Add OMP_NUM_THREADS=1 when running nompi tests
    
    * Run nompi tests without parallel
    
    * Make it possible to skip running the tests based on environment variables,
    for a quick workaround in case of more quirky problems with TavisCI in
    the future.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,24 +3,36 @@
 echo "**** run_test.sh starting"
 echo "**** mpi: $mpi"
 echo "**** python: $($PYTHON --version)"
+echo "**** SKIP_TESTS_MPI: $SKIP_TESTS_MPI"
+echo "**** SKIP_TESTS_NOMPI: $SKIP_TESTS_NOMPI"
+
 
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
+    if [[ "$SKIP_TESTS_MPI" = "yes" ]]; then
+        echo "Skipping tests for MPI build, as directed."
+        exit 0
+    fi
     for t in python/tests/*.py; do
         if [ "$(basename $t)" != "material_dispersion.py" -a "$(basename $t)" != "mpb.py" ]; then
-            echo "**** Running $(basename $t)"
+            echo "Running $(basename $t)"
             OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
         fi
     done
 else
+    if [[ "$SKIP_TESTS_NOMPI" = "yes" ]]; then
+        echo "Skipping tests for NOMPI build, as directed."
+        exit 0
+    fi
     # Mac builds are over the 50 minute time limit on Travis. Skip the serial
     # tests until we find a way to speed things up
     if [[ $(uname) == Linux ]]; then
-        # OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel -v "$PYTHON {}"
-
+        export OPENBLAS_NUM_THREADS=1
+        export OMP_NUM_THREADS=1
         for t in $(find python/tests -name "*.py" | sed /mpb/d); do
-            echo "**** Running $(basename $t)"
-            OPENBLAS_NUM_THREADS=1 $PYTHON $t
+            echo "Running $(basename $t)"
+            $PYTHON $t
         done
-
+    else
+        echo "Skipping NOMPI tests on OSX."
     fi
 fi
